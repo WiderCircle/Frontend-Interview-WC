99e35e2e9d70083382af204ecc6e1dc49c7b0cd7
Matthew Evans <matthewevans1991@gmail.com>
2023-09-29 11:43:00 -0600

    handle multiple forms

--- hayes-valley-helath-referral-form/src/pages/referral-page/referral-form.tsx
+++ hayes-valley-helath-referral-form/src/pages/referral-page/referral-form.tsx
@@ -6,82 +6,127 @@ import CakeIcon from '@mui/icons-material/Cake';
 import LanguageIcon from '@mui/icons-material/Translate';
 import PhoneIcon from '@mui/icons-material/Phone';
 import EmailIcon from '@mui/icons-material/Email';
-import LocationOnIcon from '@mui/icons-material/LocationOn';
+import DeleteIcon from '@mui/icons-material/Delete';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import WcInput from '../../app/custom-components/wc-input';
+import { IconButton, IconButtonProps, styled } from '@mui/material';
+import Collapse from '@mui/material/Collapse';
 
-function ReferralForm() {
+interface ReferralFormProps {
+    index: number;
+    expanded: boolean;
+    showUtilityButtons?: boolean;
+    onExpandClick: () => void;
+    onDeleteClick: () => void;
+}
+
+interface ExpandMoreProps extends IconButtonProps {
+    expand: boolean;
+}
+
+const ExpandMore = styled((props: ExpandMoreProps) => {
+    const { expand, ...other } = props;
+    return <IconButton {...other} />;
+})(({ theme, expand }) => ({
+    transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
+    marginLeft: 'auto',
+    transition: theme.transitions.create('transform', {
+        duration: theme.transitions.duration.shortest,
+    }),
+}));
+
+
+function ReferralForm({ index, expanded, showUtilityButtons = false, onExpandClick, onDeleteClick }: ReferralFormProps) {
     return (
-        <form style={{ backgroundColor: '#fff' }}>
+        <form style={{ backgroundColor: '#fff', marginTop: '10px' }}>
             <header>
                 <Grid container alignItems="center">
                     <Grid item xs={0.75}>
-                        <Typography variant="h4" style={{ backgroundColor: 'green', color: '#fff', paddingTop: '25px', paddingBottom: '25px' }}>1</Typography>
+                        <Typography variant="h4" style={{ backgroundColor: 'green', color: '#fff', paddingTop: '25px', paddingBottom: '25px' }}>{index + 1}</Typography>
                     </Grid>
-                    <Grid item xs={11} style={{ textAlign: 'left', paddingLeft: '10px' }}>
+                    <Grid item xs={10} style={{ textAlign: 'left', paddingLeft: '10px' }}>
                         <Typography variant="h5"> New Referral </Typography>
                     </Grid>
+                    {showUtilityButtons &&
+                        <Grid item xs={0.75} style={{ display: 'flex' }}>
+                            <IconButton
+                                onClick={onDeleteClick}>
+                                <DeleteIcon />
+                            </IconButton>
+                            <ExpandMore
+                                expand={expanded}
+                                onClick={onExpandClick} // Use the provided callback
+                                aria-expanded={expanded}
+                                aria-label="show more"
+                            >
+                                <ExpandMoreIcon />
+                            </ExpandMore>
+                        </Grid>
+                    }
                 </Grid>
             </header>
-            <Grid container>
-                <Grid item xs={0.5}></Grid>
-                <Grid item xs={11}>
-                    <Grid container spacing={2} style={{ padding: "30px" }}>
-                        <Grid item xs={6} >
-                            <WcInput
-                                placeholder="First Name"
-                                required
-                                Icon={PersonIcon}
-                            />
-                        </Grid>
-                        <Grid item xs={6}>
-                            <WcInput
-                                placeholder="Last Name"
-                                required
-                                Icon={PersonIcon}
-                            />
-                        </Grid>
-                        <Grid item xs={6}>
-                            <WcInput
-                                placeholder="Date of Birth"
-                                required
-                                Icon={CakeIcon}
-                            />
-                        </Grid>
-                        <Grid item xs={6}>
-                            <WcInput
-                                placeholder="Contact Language"
-                                required
-                                Icon={LanguageIcon}
-                            />
-                        </Grid>
-                        <Grid item xs={6}>
-                            <WcInput
-                                placeholder="Phone"
-                                required
-                                Icon={PhoneIcon}
-                            />
-                        </Grid>
-                        <Grid item xs={6}>
-                            <WcInput
-                                placeholder="Email"
-                                required
-                                Icon={EmailIcon}
-                            />
-                        </Grid>
-                        <Grid item xs={12}>
-                            <WcInput
-                                placeholder="Address"
-                                required
-                            />
-                        </Grid>
-                        <Grid item xs={12}>
-                            <WcInput
-                                placeholder="Notes/Reason"
-                            />
+            <Collapse in={expanded} timeout="auto" unmountOnExit>
+                <Grid container>
+                    <Grid item xs={0.5}></Grid>
+                    <Grid item xs={11}>
+                        <Grid container spacing={2} style={{ padding: "30px" }}>
+                            <Grid item xs={6} >
+                                <WcInput
+                                    placeholder="First Name"
+                                    required
+                                    Icon={PersonIcon}
+                                />
+                            </Grid>
+                            <Grid item xs={6}>
+                                <WcInput
+                                    placeholder="Last Name"
+                                    required
+                                    Icon={PersonIcon}
+                                />
+                            </Grid>
+                            <Grid item xs={6}>
+                                <WcInput
+                                    placeholder="Date of Birth"
+                                    required
+                                    Icon={CakeIcon}
+                                />
+                            </Grid>
+                            <Grid item xs={6}>
+                                <WcInput
+                                    placeholder="Contact Language"
+                                    required
+                                    Icon={LanguageIcon}
+                                />
+                            </Grid>
+                            <Grid item xs={6}>
+                                <WcInput
+                                    placeholder="Phone"
+                                    required
+                                    Icon={PhoneIcon}
+                                />
+                            </Grid>
+                            <Grid item xs={6}>
+                                <WcInput
+                                    placeholder="Email"
+                                    required
+                                    Icon={EmailIcon}
+                                />
+                            </Grid>
+                            <Grid item xs={12}>
+                                <WcInput
+                                    placeholder="Address"
+                                    required
+                                />
+                            </Grid>
+                            <Grid item xs={12}>
+                                <WcInput
+                                    placeholder="Notes/Reason"
+                                />
+                            </Grid>
                         </Grid>
                     </Grid>
                 </Grid>
-            </Grid>
+            </Collapse>
         </form>
     );
 }
--- hayes-valley-helath-referral-form/src/pages/referral-page/referral-page.tsx
+++ hayes-valley-helath-referral-form/src/pages/referral-page/referral-page.tsx
@@ -1,38 +1,74 @@
-import { Container, Grid, Typography, createTheme } from "@mui/material";
-import ReferralForm from "./referral-form";
-import Button from "@mui/material/Button";
+'use client'
 
+import React, { useState } from 'react';
+import { Container, Grid, Typography } from '@mui/material';
+import ReferralForm from './referral-form';
+import Button from '@mui/material/Button';
 
 export default function ReferralPage() {
+    const maxReferralForms = 5;
+    const [referralForms, setReferralForms] = useState([{ key: 1, expanded: true }]);
 
-    // const theme = createTheme();
+    const addReferralForm = () => {
+        if (referralForms.length < maxReferralForms) {
+            const updatedForms = referralForms.map((form) => {
+                form.expanded = false
+                return form;
+            });
+            updatedForms.push({ key: referralForms.length + 1, expanded: true });
+            setReferralForms(updatedForms);
+        }
+    };
 
-    // theme.typography.fontFamily = "Montserrat";
-    // 
+    const handleFormExpandClick = (index: number) => {
+        const updatedForms = [...referralForms];
+        updatedForms[index].expanded = !updatedForms[index].expanded;
+        const updatedFormsNew = [...updatedForms];
+        setReferralForms(updatedFormsNew);
+    };
+
+    const handleFormDeleteClick = (indexToRemove: number) => {
+        if (referralForms.length <= 1)
+            return;//the delete button shouldn't be shown anyway.
+        const updatedForms = referralForms.filter((form, index) => (index != indexToRemove));
+        setReferralForms(updatedForms);
+    }
+
     return (
         <Grid container spacing={0}>
-            <Grid item xs={12} style={{ backgroundColor: "#fff" }}>
-                <Container style={{ textAlign: "center" }}>
+            <Grid item xs={12} style={{ backgroundColor: '#fff' }}>
+                <Container style={{ textAlign: 'center' }}>
                     <header>
                         <Typography variant="h1">Patient Referral Form</Typography>
                         <Typography variant="h2">Hayes Valley Health San Francisco</Typography>
                     </header>
                 </Container>
             </Grid>
-            <Grid item xs={12} style={{ textAlign: "center" }}>
-                <Container maxWidth="md" style={{ textAlign: "center" }}>
+            <Grid item xs={12} style={{ textAlign: 'center' }}>
+                <Container maxWidth="md" style={{ textAlign: 'center' }}>
                     <main>
                         <Typography variant="h3">Referral Patients</Typography>
                         <Typography variant="subtitle1">You can add up to five patients at a time</Typography>
-                        <ReferralForm />
-                        <Button variant="text">+ Add another patient</Button>
-                        <Button variant="contained" style={{ display: "block", width: "100%" }}>Send Referrals</Button>
+                        {referralForms.map((form, index) => (
+                            <div key={form.key}>
+                                <ReferralForm
+                                    index={index}
+                                    showUtilityButtons={referralForms.length > 1}
+                                    expanded={form.expanded}
+                                    onExpandClick={() => handleFormExpandClick(index)}
+                                    onDeleteClick={() => handleFormDeleteClick(index)}
+                                />
+                            </div>
+                        ))}
+                        <Button variant="text" onClick={addReferralForm}>
+                            + Add another patient
+                        </Button>
+                        <Button variant="contained" style={{ display: 'block', width: '100%' }}>
+                            Send Referrals
+                        </Button>
                     </main>
                 </Container>
             </Grid>
         </Grid>
-    )
-
-
-    // return <>Referral page!</>
+    );
 }

f51b3e7ac65534406d54ae131598e27c0105ddc9
Matthew Evans <matthewevans1991@gmail.com>
2023-09-29 09:27:40 -0600

    add page layout

---
+++ hayes-valley-helath-referral-form/src/pages/referral-page/referral-form.tsx
@@ -0,0 +1,8 @@
+
+
+
+export default function ReferralForm() {
+    return (
+        <form>Referral Form</form>
+    )
+}
---
+++ hayes-valley-helath-referral-form/src/pages/referral-page/referral-page.tsx
@@ -0,0 +1,38 @@
+import { Container, Grid, Typography, createTheme } from "@mui/material";
+import ReferralForm from "./referral-form";
+import Button from "@mui/material/Button";
+
+
+export default function ReferralPage() {
+
+    // const theme = createTheme();
+
+    // theme.typography.fontFamily = "Montserrat";
+    // 
+    return (
+        <Grid container spacing={0}>
+            <Grid item xs={12} style={{ backgroundColor: "#fff" }}>
+                <Container style={{ textAlign: "center" }}>
+                    <header>
+                        <Typography variant="h1">Patient Referral Form</Typography>
+                        <Typography variant="h2">Hayes Valley Health San Francisco</Typography>
+                    </header>
+                </Container>
+            </Grid>
+            <Grid item xs={12} style={{ textAlign: "center" }}>
+                <Container maxWidth="sm" style={{ textAlign: "center" }}>
+                    <main>
+                        <Typography variant="h3">Referral Patients</Typography>
+                        <Typography variant="subtitle1">You can add up to five patients at a time</Typography>
+                        <ReferralForm />
+                        <Button variant="text">+ Add another patient</Button>
+                        <Button variant="contained" style={{ display: "block", width: "100%" }}>Send Referrals</Button>
+                    </main>
+                </Container>
+            </Grid>
+        </Grid>
+    )
+
+
+    // return <>Referral page!</>
+}

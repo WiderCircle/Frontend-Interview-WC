f05fb3c08bae619947c9c2b2dd629b2451b5db5c
Jules Benjamin <7259703+julietkb@users.noreply.github.com>
2023-10-23 16:30:51 -0400

    Create FormikConfig

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -13,50 +13,93 @@ const Main: NextPage = () => {
   const [message, setMessage] = useState('');
   const [submitted, setSubmitted] = useState(false);
 
-  const initialValues = {
-    firstName: '',
-    lastName: '',
-    dateOfBirth: '',
-    contactLanguage: '',
-    phone: '',
-    email: '',
-    address: '',
-    notes: '',
+  type FormikConfig = {
+    initialValue: string,
+    placeholder: string,
+    schema: yup.Schema,
+    type: string,
+    width?: string,
+  }
+  const formikConfig: { [input: string]: FormikConfig } = {
+    firstName: {
+      initialValue: '',
+      placeholder: "First Name",
+      schema: yup.string().required('First name is required'),
+      type: "text",
+    },
+    lastName: {
+      initialValue: '',
+      placeholder: "Last Name",
+      schema: yup.string().required('Last name is required'),
+      type: "text",
+    },
+    dateOfBirth: {
+      initialValue: '',
+      placeholder: "Date of Birth",
+      schema: yup.date().required('Date of birth is required'),
+      type: "date",
+    },
+    contactLanguage: {
+      initialValue: '',
+      placeholder: "Contact Language",
+      schema: yup.string().required('Language is required'),
+      type: "text",
+    },
+    phone: {
+      initialValue: '',
+      placeholder: "Phone",
+      schema: yup
+        .string()
+        .required('Phone number is required')
+        .matches(/^[0-9]*$/, 'Must be a valid phone number')
+        .min(10, 'Must be a valid phone number')
+        .max(10, 'Must be a valid phone number'),
+      type: "tel",
+    },
+    email: {
+      initialValue: '',
+      placeholder: "Email",
+      schema: yup
+        .string()
+        .email('Must be a valid email')
+        .required('Email is required'),
+      type: "email",
+    },
+    address: {
+      initialValue: '',
+      placeholder: "Address",
+      schema: yup.string().required('Address is required'), // todo: address autocomplete
+      type: "text",
+      width: "100%",
+    },
+    notes: {
+      initialValue: '',
+      placeholder: "Notes",
+      schema: yup.string(),
+      type: "text",
+      width: "100%",
+    },
   };
-
-  const validationSchema = yup.object({
-    firstName: yup.string().required('First name is required'),
-    lastName: yup.string().required('Last name is required'),
-    dateOfBirth: yup.date().required('Date of birth is required'),
-    contactLanguage: yup.string().required('Language is required'),
-    phone: yup
-      .string()
-      .required('Phone number is required')
-      .matches(/^[0-9]*$/, 'Must be a valid phone number')
-      .min(10, 'Must be a valid phone number')
-      .max(10, 'Must be a valid phone number'),
-    email: yup
-      .string()
-      .email('Must be a valid email')
-      .required('Email is required'),
-    address: yup.string().required('Address is required'), // todo: address autocomplete
-    notes: yup.string(),
-  });
 
   return (
     <Formik
-      initialValues={initialValues}
+      initialValues={Object.keys(formikConfig).reduce((obj, key) => {
+        obj[key] = formikConfig[key].initialValue;
+        return obj;
+      }, {})}
       onSubmit={() => {
         // todo: handle multiple referrals in success message
         setMessage("Success! You have submitted a patient referral. You will be notified once they've been approved.");
         setSubmitted(true);
       }}
-      validationSchema={validationSchema}
+      validationSchema={yup.object(Object.keys(formikConfig).reduce((obj, key) => {
+        obj[key] = formikConfig[key].schema;
+        return obj;
+      }, {}))}
     >
       {(formik) => {
-        const isInvalidForm = Object.values(formik.errors).some(error => error !== null || error !== "");
-        const fullName = formik.values.firstName && formik.values.lastName
-          ? formik.values.firstName + " " + formik.values.lastName
+        const fullName = formik.values["firstName"] && formik.values["lastName"]
+          ? formik.values["firstName"] + " " + formik.values["lastName"]
           : "New Referral";
         const patientNumber = 1;
 
@@ -66,11 +109,9 @@ const Main: NextPage = () => {
               <h1>Patient Referral Form</h1>
               <h2>Hayes Valley Health San Francisco</h2>
             </div>
-
             <div className={styles.formPage}>
               <h1>Referral Patients</h1>
               <h2>You can add up to five patients at a time</h2>
-
               <div className={styles.form}>
                 <div className={styles.formHeader}>
                   <p>{patientNumber}</p>
@@ -82,14 +123,11 @@ const Main: NextPage = () => {
             - create reusable form component (for up to 5 patients) */}
                 <form onSubmit={formik.handleSubmit}>
                   <div className={styles.formInputs}>
-                    <FormInput type="text" name="firstName" placeholder="First Name" />
-                    <FormInput type="text" name="lastName" placeholder="Last Name" />
-                    <FormDateInput name="dateOfBirth" placeholderText="Date of Birth" />
-                    <FormInput type="text" name="contactLanguage" placeholder="Contact Language" />
-                    <FormInput type="phone" name="phone" placeholder="Phone" />
-                    <FormInput type="email" name="email" placeholder="Email" />
-                    <FormInput type="address" name="address" placeholder="Address" width="100%" />
-                    <FormInput type="text" name="notes" placeholder="Notes" width="100%" />
+                    {Object.entries(formikConfig).map(([name, input]) => (
+                      input.type === "date" 
+                      ? <FormDateInput name={name} placeholderText={input.placeholder} />
+                      : <FormInput type={input.type} name={name} placeholder={input.placeholder} />
+                    ))}
                   </div>
                   {/* todo: 
                   - make "+ add another patient" button that duplicates form
